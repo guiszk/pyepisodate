758805f96b07db38426ac642d06a2c012b1fd69b
guiszk <guiszk@protonmail.com>
2021-06-12 17:40:52 -0300

    Added long description

--- setup.py
+++ setup.py
@@ -6,17 +6,22 @@ about = dict()
 with open(os.path.join(here, 'pyepisodate', '__version__.py'), 'r') as f:
     exec(f.read(), about)
 
+with open('README.md', 'rt') as f:
+    long_description = f.read()
+
 setup(
   name = 'pyepisodate',
   packages = ['pyepisodate'],
   version=about['__version__'],
   license='BSD',
   description = 'A python wrapper for the EpisoDate API',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
   author = 'guiszk',
   author_email = 'guiszk@protonmail.com',
   url = 'https://github.com/guiszk/pyepisodate',
   download_url = 'https://github.com/guiszk/pyepisodate/dist/pyepisodate-0.4.2.tar.gz',
-  keywords = ['api', 'episodate', 'api'],
+  keywords = ['api', 'episodate', 'python'],
   install_requires=['requests'],
   classifiers=[
     'Development Status :: 3 - Alpha',
