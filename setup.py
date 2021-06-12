0767bdbf1813e547e9c8b851601388c51a54640c
guiszk <guiszk@protonmail.com>
2021-06-12 17:32:22 -0300

    Added files

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = dict()
+with open(os.path.join(here, 'pyepisodate', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setup(
+  name = 'pyepisodate',
+  packages = ['pyepisodate'],
+  version=about['__version__'],
+  license='BSD',
+  description = 'A python wrapper for the EpisoDate API',
+  author = 'guiszk',
+  author_email = 'guiszk@protonmail.com',
+  url = 'https://github.com/guiszk/pyepisodate',
+  download_url = 'https://github.com/guiszk/pyepisodate/v_01.tar.gz',
+  keywords = ['api', 'episodate', 'api'],
+  install_requires=['requests'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+  ],
+)
